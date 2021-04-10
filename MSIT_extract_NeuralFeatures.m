8ff078d16d5aed87b178b8aeb6ac00285bcd1983
ibasu2 <74792122+ibasu2@users.noreply.github.com>
2021-04-10 14:22:36 -0400

    Update MSIT_extract_NeuralFeatures.m

--- MSIT_extract_NeuralFeatures.m
+++ MSIT_extract_NeuralFeatures.m
@@ -1,83 +1,72 @@
 clear all
 
 %% This code creates neural features and state estimates for each subject dataset and saves them for encoder-decoder modeling 
-MainPath='D:\BasuDDrive\MSIT paper\Neural Data\'; % Specify Data Path
-Subject='P01'; % Specify subject code
+MainPath='E:\MSIT data for NatureBME\Trial LFP\'; % Specify Data Path
+savePath='E:\MSIT data for NatureBME\PSD\'; % Specify folder where power spectrum will be stored
 
 %% Convert input LFP trials saved in Fieldtrip format  to Power Spectrum. This requires the Fieldtrip toolbox
 
-%stimType='No_Stim2';
-stimType='STIM';
-filename=['FT data\Trial LFP\BIPOLFieldTripFormat_AlignedToImagePresent_',Subject,'MSIT',stimType,'.mat'];
-fileInterictal=['interictal channels\',Subject,'Channels_InterictalActivity_NS2.mat'];
+filename=dir([MainPath,'*.mat']);
 
-load([MainPath,filename]);
-load([MainPath,fileInterictal]);
-
-ChannelPairNames = [ChannelPairNamesBank1 ; ChannelPairNamesBank2];
-for c=1:length(ft_data3.label)
-   chIdx = str2num(ft_data3.label{c});
-   ft_data3.label{c} = ChannelPairNames(chIdx,:);
+for ff=1:length(filename)
+    load([MainPath,filename(ff).name]);
+    
+    ChannelPairNames = [ChannelPairNamesBank1 ; ChannelPairNamesBank2];
+    for c=1:length(ft_data3.label)
+        chIdx = str2num(ft_data3.label{c});
+        ft_data3.label{c} = ChannelPairNames(chIdx,:);
+    end
+    
+    % Get rid of sampleinfo if it exists.
+    ft_data3 =  rmfield(ft_data3,'sampleinfo');
+    
+    % Notch filter the trials.
+    cfg = [];
+    cfg.bsfilter = 'yes';
+    cfg.bsfreq = [115 135];
+    cfg.bsfiltord = 4;
+    
+    addpath 'E:\matlab codes\fieldtrip-master'
+    ft_data3 = ft_preprocessing(cfg,ft_data3);
+    
+    % PSD calculation
+    cfg = [];
+    cfg.output = 'pow';
+    cfg.keeptrials = 'yes';
+    cfg.method = 'wavelet';
+    cfg.foi = [2:50 55:5:200];
+    cfg.toi = [-1.9:0.01:3.9];
+    ft_freq = ft_freqanalysis(cfg,ft_data3);
+    
+    channel_label=ft_freq.label;
+    id=strfind(filename(ff).name,'_P');
+    Subject=filename(ff).name(id+1:length(filename(ff).name));
+    
+    if (stim)
+        % Run if STIM data
+        BlockNumber = BlockNumber';
+        BlockType = BlockType';
+        
+        StimLocation = cell(size(TrialDet,1),1);
+        for b=1:length(BlockStimLocation)
+            StimLocation(BlockNumber==b) = deal(BlockStimLocation(b));
+        end
+        save([savePath,'PSD_',Subject],'ft_data3','ft_freq','ch_ictal','channel_label','TrialDet','BlockStimLocation','StimLocation','BlockNumber','BlockType','ParcellationValues','-v7.3');
+        
+    else
+        save([savePath,'PSD_',Subject],'ft_data3','ft_freq','ch_ictal','channel_label','TrialDet','ParcellationValues','-v7.3');
+    end
 end
-
-% Get rid of sampleinfo if it exists.
-ft_data3 =  rmfield(ft_data3,'sampleinfo');
-
-% Notch filter the trials.
-cfg = [];
-cfg.bsfilter = 'yes';
-cfg.bsfreq = [115 135];
-cfg.bsfiltord = 4;
-
-addpath 'D:\C Drive\matlab codes\fieldtrip-master'
-ft_data3 = ft_preprocessing(cfg,ft_data3);
-
-% PSD calculation
-cfg = [];
-cfg.output = 'pow';
-cfg.keeptrials = 'yes';
-cfg.method = 'wavelet';
-cfg.foi = [2:50 55:5:200];
-cfg.toi = [-1.9:0.01:3.9];
-ft_freq = ft_freqanalysis(cfg,ft_data3);
-
-channel_label=ft_freq.label;
-
-savePath=[MainPath,'PSD\'];
-
-save([savePath,Subject,'PSD',stimType,'.mat'],'ft_data3','ft_freq','ch_ictal','channel_label','TrialDet','-v7.3');
-
-% Run if STIM data
-BlockNumber = BlockNumber';
-BlockType = BlockType';
-
-%% Uncomment the specific P#
-%BlockStimLocation = {'None','None','LDacc1','LDacc1','LDacc2','LDacc2','None'}; %P02 CL
-%BlockStimLocation = {'None','LVentral','RVentral','LMiddle','RMiddle','None'}; %P08
-%BlockStimLocation = {'None','R Ventral','L Dorsal','BAD','R Dorsal','LVentral','None'};%P09
-%BlockStimLocation = {'None','L Dorsal','L Ventral','L Dorsal'}; %P10
-%BlockStimLocation =  {'None','None','Bad','Bad','Bad','R Ventral','RDorsal','L Ventral','L Middle','L Dorsal'}; %P11
-%BlockStimLocation =  {'None','None','R Ventral','R Dorsal','L Ventral','LDorsal','R Dorsal Fix','R Dorsal delay','R Dorsal','None'};% P12
-%BlockStimLocation =  {'None','None','R Ventral','R Dorsal','L Ventral','L
-%Dorsal','L Dorsal Fix','L Dorsal delay','R Dorsal Fix','R Dorsaldelay','None'}; % P13
-%BlockStimLocation ={'None','None','None','R Dorsal','L Dorsal','L Dorsal'};% P14, part1
-%BlockStimLocation ={'None','R Ventral','R Dorsal','R Dorsal'};% P14, part2
-%BlockStimLocation ={'None','None','aborted','single pulse','R Dorsal','RDorsal','R Ventral','None'}; % P15
-%BlockStimLocation={'None','RDorsal','RDorsal','LDorsal','Rventral','RDorsal','None','RDorsal_HGP','RVentral_HGP','None'};%P17
-
-StimLocation = cell(size(TrialDet,1),1);
-for b=1:length(BlockStimLocation)
-   StimLocation(BlockNumber==b) = deal(BlockStimLocation(b)); 
-end
-save([savePath,Subject,'PSD',stimType,'.mat'],'ft_data3','ft_freq','ch_ictal','channel_label','TrialDet','BlockStimLocation','StimLocation','BlockNumber','BlockType','-v7.3');
 
 %% Neural feature matrix and behavior
 
 clear all
-Subject='P01';
-MainPath='D:\BasuDDrive\MSIT paper\Neural Data\';
+SubjectId=1:21;
+MainPath='E:\MSIT data for NatureBME\Trial LFP\PSD\';
 
-files=ls(['PSD\',Subject,'*.mat']);
+for pp=1:length(SubjectId)
+    
+files=ls([MainPath,'PSD_P',num2str(SubjectId(pp)),'*.mat']);
 
 % Behavior
 RT=[];
@@ -90,7 +79,7 @@ id_rt=12;
 id_conflict=14;
 
 for ff=1:size(files,1)
-    load([MainPath,'PSD\',files(ff,:)],'ch_ictal','TrialDet');
+    load([MainPath,files(ff,:)],'ch_ictal','TrialDet');
     RT=[RT;TrialDet(:,id_rt);nan(Nnan,1)];
     seq=[seq,length(TrialDet(:,id_rt)),Nnan];
     seq_id=[seq_id,1,0];
@@ -102,19 +91,20 @@ I=I(1:length(I)-Nnan);
 seq=seq(1:end-1);
 seq_id=seq_id(1:end-1);
 
-figure;plot(RT)
 % Reject channels with interictal activity and find common channels 
 channels_exclude=[];
-load([MainPath,'PSD\',files(1,:)],'channel_label');
+load([MainPath,files(1,:)],'channel_label');
 channels_common=channel_label;
 
 for fl=1:size(files,1)
     
-    load([MainPath,'PSD\',files(fl,:)],'channel_label','ch_ictal');
+    load([MainPath,files(fl,:)],'channel_label','ch_ictal','ParcellationValues');
     channels_common=intersect(channels_common,channel_label);
     channels_exclude=union(channels_exclude,mat2cell(ch_ictal,ones(1,size(ch_ictal,1)),size(ch_ictal,2)));
 end
 channel_name=setdiff(channels_common,channels_exclude);
+[~,id1,id2]=intersect(channel_label,channel_name);
+Regions=ParcellationValues(id1,8);
 
 % Neural features finally
 F=[4,8;8,15;15,30;30,55;70,110;130,200]; % Frequency bands
@@ -125,7 +115,7 @@ S=[];
 
 for fl=1:size(files,1)
     
-    load([MainPath,'PSD\',files(fl,:)],'ft_freq','TrialDet');
+    load([MainPath,files(fl,:)],'ft_freq','TrialDet');
       
     trIdx=1:length(TrialDet);
     
@@ -206,25 +196,8 @@ for i=1:K
     temp=XPos{i};xm(i,1)=temp(1);xm(i,2)=temp(2);
     temp=SPos{i};xb(i,1)=temp(1,1);xb(i,2)=temp(2,2);
 end
-subplot(2,1,1);ay_plot_bound(1,(1:K),xm(:,1),(xm(:,1)-2*sqrt(xb(:,1)))',(xm(:,1)+2*sqrt(xb(:,1)))');hold on;plot(Yn)
-subplot(2,1,2);ay_plot_bound(1,(1:K),xm(:,2),(xm(:,2)-2*sqrt(xb(:,2)))',(xm(:,2)+2*sqrt(xb(:,2)))');
 
-%% Saving data for modeling
-stimType='NoStim_Stim';
-save([MainPath,'Extracted Features\',Subject,'_features_',stimType,'.mat'],'Y','Ys','YL','seq','seq_id','RT','In','channel_name','Param','XPos','SPos','XSmt','SSmt','YP');
+% Saving data for modeling
+save([MainPath,'Extracted Features\P',num2str(pp),'_features.mat'],'Y','Ys','YL','seq','seq_id','RT','In','channel_name','Param','XPos','SPos','XSmt','SSmt','YP','Regions');
 
-%% Getting parcellations
-clear all
-load('D:\BasuDDrive\MSIT paper\Neural Data\FT data\parcellation\MG120_NS1.mat')
-Reference_channels=[];
-ChannelPairs=[ChannelPairNamesBank1;ChannelPairNamesBank2];
-for ii=1:length(ChannelPairs)
-Reference_channels{ii}=ChannelPairs(ii,:);
 end
-
-load('P01_features_No_Stim.mat', 'channel_name')
-[~,id1,id2]=intersect(Reference_channels,channel_name);
-
-Regions=ParcellationValues(id1,8);
-
-
